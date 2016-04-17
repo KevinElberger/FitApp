3b5d9caf539a4f98e3491940b1283dfec3d4a99f
Kevin Elberger <kevelberger@gmail.com>
2016-04-16 21:54:06 -0700

    edit show page for lifts

--- resources/views/workouts/show.blade.php
+++ resources/views/workouts/show.blade.php
@@ -6,30 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>FitApp - Track and Log Your Workouts</title>
 
-    <link href="{{URL::asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="{{URL::asset('fonts/font-awesome.min.css')}}" type="text/css" media="screen">
+    <link rel="stylesheet" href="/fonts/font-awesome.min.css" type="text/css" media="screen">
     <!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
-    <link href="{{URL::asset('css/material.min.css')}}" rel="stylesheet">
-    <link href="{{URL::asset('css/ripples.min.css')}}" rel="stylesheet">
+    <link href="/css/material.min.css" rel="stylesheet">
+    <link href="/css/ripples.min.css" rel="stylesheet">
     {{--<link href="{{URL::asset('css/main.css')}}" rel="stylesheet">--}}
-    <link href="{{URL::asset('css/responsive.css')}}" rel="stylesheet">
-    <link href="{{URL::asset('css/animate.min.css')}}" rel="stylesheet">
-    <link href="{{URL::asset('css/jquery-ui.css')}}" rel="stylesheet">
-    <script src="{{URL::asset('js/jquery-2.1.4.min.js')}}"></script>
-    <script src="{{URL::asset('js/jquery.mmenu.min.all.js')}}"></script>
-    <script src="{{URL::asset('js/jquery.inview.min.js')}}"></script>
-    <script src="{{URL::asset('js/jquery-ui.js')}}"></script>
-    <script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
-    <script src="{{URL::asset('js/ripples.min.js')}}"></script>
-    <script src="{{URL::asset('js/material.min.js')}}"></script>
-    <script src="{{URL::asset('js/wow.js')}}"></script>
-    <script src="{{URL::asset('js/count-to.js')}}"></script>
-    <script src="{{URL::asset('js/main.js')}}"></script>
-    <script src="{{URL::asset('js/classie.js')}}"></script>
-    <script src="{{URL::asset('js/jquery.nav.js')}}"></script>
-    <script src="{{URL::asset('js/smooth-on-scroll.js')}}"></script>
-    <script src="{{URL::asset('js/smooth-scroll.js')}}"></script>
+    <link href="/css/responsive.css" rel="stylesheet">
+    <link href="/css/animate.min.css" rel="stylesheet">
+    <link href="/css/jquery-ui.css" rel="stylesheet">
     <script src="//d3js.org/d3.v3.min.js"></script>
     <script>
         var arr = [];
@@ -74,8 +60,20 @@
         <svg id="visualization" width="600" height="400"></svg>
     </div>
 </div>
-
-
+<script src="/js/jquery-2.1.4.min.js"></script>
+<script src="/js/jquery.mmenu.min.all.js"></script>
+<script src="/js/jquery.inview.min.js"></script>
+<script src="/js/jquery-ui.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/ripples.min.js"></script>
+<script src="/js/material.min.js"></script>
+<script src="/js/wow.js"></script>
+<script src="/js/count-to.js"></script>
+<script src="/js/main.js"></script>
+<script src="/js/classie.js"></script>
+<script src="/js/jquery.nav.js"></script>
+<script src="/js/smooth-on-scroll.js"></script>
+<script src="/js/smooth-scroll.js"></script>
 <script type="text/javascript">
     $(function() {
         // This command is used to initialize some elements and make them work properly.
