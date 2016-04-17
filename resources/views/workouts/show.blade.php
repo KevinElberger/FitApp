6a95faf1b8da96bdc6a93e36975262087902b067
Kevin Elberger <kevelberger@gmail.com>
2016-04-16 23:55:08 -0700

    placing scripts at bottom of body

--- resources/views/workouts/show.blade.php
+++ resources/views/workouts/show.blade.php
@@ -16,21 +16,6 @@
     <link href="/css/responsive.css" rel="stylesheet">
     <link href="/css/animate.min.css" rel="stylesheet">
     <link href="/css/jquery-ui.css" rel="stylesheet">
-    <script src="/js/jquery-2.1.4.min.js"></script>
-    <script src="/js/jquery.mmenu.min.all.js"></script>
-    <script src="/js/jquery.inview.min.js"></script>
-    <script src="/js/jquery-ui.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-    <script src="/js/ripples.min.js"></script>
-    <script src="/js/material.min.js"></script>
-    <script src="/js/wow.js"></script>
-    <script src="/js/count-to.js"></script>
-    <script src="/js/main.js"></script>
-    <script src="/js/classie.js"></script>
-    <script src="/js/jquery.nav.js"></script>
-    <script src="/js/smooth-on-scroll.js"></script>
-    <script src="/js/smooth-scroll.js"></script>
-    <script src="//d3js.org/d3.v3.min.js"></script>
     <script>
         var arr = [];
     </script>
@@ -74,7 +59,21 @@
         <svg id="visualization" width="600" height="400"></svg>
     </div>
 </div>
-
+<script src="/js/jquery-2.1.4.min.js"></script>
+<script src="/js/jquery.mmenu.min.all.js"></script>
+<script src="/js/jquery.inview.min.js"></script>
+<script src="/js/jquery-ui.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/ripples.min.js"></script>
+<script src="/js/material.min.js"></script>
+<script src="/js/wow.js"></script>
+<script src="/js/count-to.js"></script>
+<script src="/js/main.js"></script>
+<script src="/js/classie.js"></script>
+<script src="/js/jquery.nav.js"></script>
+<script src="/js/smooth-on-scroll.js"></script>
+<script src="/js/smooth-scroll.js"></script>
+<script src="//d3js.org/d3.v3.min.js"></script>
 <script type="text/javascript">
     $(function() {
         // This command is used to initialize some elements and make them work properly.
