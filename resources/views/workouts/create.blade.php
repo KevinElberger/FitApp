0fd33e768c5f466bd423878f165b1d8a876019a7
Kevin Elberger <kevelberger@gmail.com>
2016-04-16 21:43:12 -0700

    tweaks css for workout creation

--- resources/views/workouts/create.blade.php
+++ resources/views/workouts/create.blade.php
@@ -10,26 +10,12 @@
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="fonts/font-awesome.min.css" type="text/css" media="screen">
     <!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
-    <link href="css/material.min.css" rel="stylesheet">
-    <link href="css/ripples.min.css" rel="stylesheet">
+    <link href="/css/material.min.css" rel="stylesheet">
+    <link href="/css/ripples.min.css" rel="stylesheet">
     {{--<link href="{{URL::asset('css/main.css')}}" rel="stylesheet">--}}
-    <link href="css/responsive.css" rel="stylesheet">
-    <link href="css/animate.min.css" rel="stylesheet">
-    <link href="css/jquery-ui.css" rel="stylesheet">
-    <script src="js/jquery-2.1.4.min.js"></script>
-    <script src="js/jquery.mmenu.min.all.js"></script>
-    <script src="js/jquery.inview.min.js"></script>
-    <script src="js/jquery-ui.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/ripples.min.js"></script>
-    <script src="js/material.min.js"></script>
-    <script src="js/wow.js"></script>
-    <script src="js/count-to.js"></script>
-    <script src="js/main.js"></script>
-    <script src="js/classie.js"></script>
-    <script src="js/jquery.nav.js"></script>
-    <script src="js/smooth-on-scroll.js"></script>
-    <script src="js/smooth-scroll.js"></script>
+    <link href="/css/responsive.css" rel="stylesheet">
+    <link href="/css/animate.min.css" rel="stylesheet">
+    <link href="/css/jquery-ui.css" rel="stylesheet">
 </head>
 
 <body>
@@ -107,10 +93,20 @@
             </div>
         {{ Form::close() }}
 </div>
-
-
-
-
+<script src="/js/jquery-2.1.4.min.js"></script>
+<script src="/js/jquery.mmenu.min.all.js"></script>
+<script src="/js/jquery.inview.min.js"></script>
+<script src="/js/jquery-ui.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/ripples.min.js"></script>
+<script src="/js/material.min.js"></script>
+<script src="/js/wow.js"></script>
+<script src="/js/count-to.js"></script>
+<script src="/js/main.js"></script>
+<script src="/js/classie.js"></script>
+<script src="/js/jquery.nav.js"></script>
+<script src="/js/smooth-on-scroll.js"></script>
+<script src="/js/smooth-scroll.js"></script>
 <script type="text/javascript">
     $(function() {
         // This command is used to initialize some elements and make them work properly
