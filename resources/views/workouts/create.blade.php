30b807d0ec4512e2351e996bebb7dbaf54e8188b
Kevin Elberger <kevelberger@gmail.com>
2016-04-16 21:45:03 -0700

    quick fix

--- resources/views/workouts/create.blade.php
+++ resources/views/workouts/create.blade.php
@@ -6,13 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>FitApp - Track and Log Your Workouts</title>
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="fonts/font-awesome.min.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="/fonts/font-awesome.min.css" type="text/css" media="screen">
     <!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
     <link href="/css/material.min.css" rel="stylesheet">
     <link href="/css/ripples.min.css" rel="stylesheet">
-    {{--<link href="{{URL::asset('css/main.css')}}" rel="stylesheet">--}}
     <link href="/css/responsive.css" rel="stylesheet">
     <link href="/css/animate.min.css" rel="stylesheet">
     <link href="/css/jquery-ui.css" rel="stylesheet">
